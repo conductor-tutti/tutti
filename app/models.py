078d4aaedfda4da086d9f6913a5e3e7123f363f8
Seulkiming <conductor@tutti.kr>
2014-08-07 20:46:30 +0900

    new class: Comment

--- app/models.py
+++ app/models.py
@@ -8,3 +8,15 @@ class Article(db.Model):
     author = db.Column(db.String(255))
     category = db.Column(db.String(255))
     date_created = db.Column(db.DateTime(), default=db.func.now())
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Columm(db.Integer, db.ForeignKey("article.id"))
+    article = db.relationship("Article",
+        backref=db.backref("comments", cascade="all, delete-orphan", lazy="dynamic"))
+    author = db.Column(db.String(255))
+    email = d.Column(db.String(255))
+    password = db.Column(db.String(255))
+    content = db.Column(db.Text())
+    date_created = db.Column(db.Datetime(), default=db.func.now())
+    
