dac1ed78d58bb2110449884da01a6b8aa084edfa
soyoung <incrediblesy@gmail.com>
2014-09-18 19:13:41 +0900

    location 추가

--- app/models.py
+++ app/models.py
@@ -37,41 +37,41 @@ class Musician(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"))
     user = db.relationship("User",
-        backref=db.backref("musician", cascade="all, delete-orphan", lazy="dynamic"))
-    
-    # each musician has its own category_id
+        backref=db.backref("musicians_user", cascade="all, delete-orphan", lazy="dynamic"))
+
     category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
-    category = db.relationship("Category", backref=db.backref("musician", cascade="all, delete-orphan", lazy="dynamic"))
-    
-    # and major_id too
+    category = db.relationship("Category", backref=db.backref("categorys_musician", cascade="all, delete-orphan", lazy="dynamic"))
+
     major_id = db.Column(db.Integer, db.ForeignKey("major.id"))
-    major = db.relationship("Major", backref=db.backref("musician", cascade="all, delete-orphan", lazy="dynamic"))
+    major = db.relationship("Major", backref=db.backref("majors_musician", cascade="all, delete-orphan", lazy="dynamic"))
+
+    locations_id = db.Column(db.Integer, db.ForeignKey("major.id"))
+    locations = db.relationship("Locations", backref=db.backref("locations_musician", cascade="all, delete-orphan", lazy="dynamic"))
+
     phrase = db.Column(db.String(255))
     photo = db.Column(db.String(255))
     created_on = db.Column(db.DateTime, default=db.func.now())
     updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
-    
-    # Soyoung'll initialize Location table
-    ## location_id = db.Column(db.Integer, db.ForeignKey("location.id"))
-    ## location = db.relationship()
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(40))
-    
-    # musicians have same category_id will be updated in this column
-    musicians = db.relationship("Musician")
     created_on = db.Column(db.DateTime, default=db.func.now())
     updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
 
 class Major(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(40))
-    
-    # each major has its mother category
-    category_id = db.Column(db.Integer)
-    
-    # same as musicians in Category table
-    musicians = db.relationship("Musician")
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+    category = db.relationship("Category", backref=db.backref("categorys_major", cascade="all, delete-orphan", lazy="dynamic"))
+
     created_on = db.Column(db.DateTime, default=db.func.now())
     updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+class Locations(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(40))
+    upper_id = db.Column(db.Integer)
+    
