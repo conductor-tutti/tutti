c766bfd84cab3ef3090ec758250929fe13df069d
Seulkiming <conductor@tutti.kr>
2014-09-18 21:45:21 +0900

    comment moved

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 class Config(object):
+    # give your own values to 2 variables below
     FACEBOOK_APP_ID = "944323692250762"
     FACEBOOK_APP_SECRET = "207996ac76593a7fe2dd9930ecb79ce5"
     SECRET_KEY = "xrdtfvbyuhnjimuygtfrdessdfnhhmjjygh65hrytrytr"
@@ -11,5 +12,4 @@ class Production(Config):
     ADMIN = "tutti.conductor@gmail.com"
     SQLALCHEMY_DATABASE_URI = "mysql+gaerdbms:///tuttidb?instance=tutti-alpha-seulki:tuttinstance"
     migration_directory = "migrations"
-    # give your own values to 2 variables below
-
+    
