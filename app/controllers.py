2d11a8e310ba457bf3ef1277eee650743b9f73ef
Seulkiming <conductor@tutti.kr>
2014-09-18 18:50:29 +0900

    unnecessary handlers and modules were deleted

--- app/controllers.py
+++ app/controllers.py
@@ -1,9 +1,9 @@
 #-*-coding:utf-8-*-
 from app import app, db
 from sqlalchemy import desc
-from app.models import Article, Comment, User, Musician, Category, Major
+from app.models import User, Musician, Category, Major
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.forms import ArticleForm, CommentForm
+from app.forms import MusicianProfileForm
 from flask import jsonify, make_response, render_template, session, request, redirect, url_for, flash, g
 from google.appengine.api import images
 from werkzeug.http import parse_options_header
@@ -36,102 +36,6 @@ def index():
     index = {}
     index["musician_list"] = Musician.query.order_by(desc(Musician.created_on)).limit(4)
     return render_template("index.html", index=index, active_tab="index")
-
-@app.route("/total_article_num")
-def total_article_num():
-    article_num = db.session.query(Article).count()
-    return jsonify(article_num=article_num)
-
-@app.route("/more_article")
-def more_article():
-    current_row = int(request.args.get("current_row"))
-    article_num = int(request.args.get("article_num"))
-    more_article = db.session.query(Article).order_by(desc(Article.date_created))[current_row:current_row+1]
-    data = {}
-    data["article"] = []
-    temp = {}
-    for article in temp["title"]: 
-        temp["id"] = article.id
-        temp["title"] = article.title
-        temp["content"] = article.content
-        temp["author"] = article.author
-        temp["category"] = article.category
-        temp["date_created"] = article.date_created
-
-        data["article"].append(temp)
-        temp = {}
-    return jsonify(data)
-
-@app.route("/article/create/", methods=["GET", "POST"])
-def article_create():
-    form = ArticleForm()
-    if request.method == "GET":
-        return render_template("article/create.html", form=form, active_tab="article_create")
-    elif request.method == "POST":
-        if form.validate_on_submit():
-            article = Article(
-                title=form.title.data,
-                author=form.author.data,
-                category=form.category.data,
-                content=form.content.data
-            )
-            db.session.add(article)
-            db.session.commit()
-            flash(u"게시물이 작성되따능.", "success")
-            return redirect(url_for("index"))
-
-@app.route("/article/detail/<int:article_id>", methods=["GET"])
-def article_detail(article_id):
-    article = Article.query.get(article_id)
-    comments = article.comments.order_by(desc(Comment.date_created)).all()
-    return render_template("article/detail.html", article=article, comments=comments)
-
-
-@app.route("/comment/create/<int:article_id>", methods=["GET", "POST"])
-def comment_create(article_id):
-    form = CommentForm()
-    if request.method == "GET":
-        return render_template("article/create.html", form=form)
-    elif request.method == "POST":
-        if form.validate_on_submit():
-            comment = Comment(
-                author=form.author.data,
-                email=form.email.data,
-                content=form.content.data,
-                password=form.password.data,
-                article=Article.query.get(article_id)
-            )
-            db.session.add(comment)
-            db.session.commit()
-            flash(u"댓글 작성해따능.", "success")
-            return redirect(url_for("article_detail", id=article_id))
-        return render_template("article/create.html", form=form)
-
-@app.route("/article/update/<int:article_id>", methods=["GET", "POST"])
-def article_update(article_id):
-    article = Article.query.get(article_id)
-    form = ArticleForm(request.form, obj=article)
-    if request.method == "GET":
-        return render_template("article/update.html", form=form)
-    elif request.method == "POST":
-        if form.validate_on_submit():
-            form.populate_obj(article)
-            db.session.commit()
-            return redirect(url_for("article_detail", article_id=article_id))
-        return render_template("article/update.html", form=form)
-
-@app.route("/article/delete/<int:article_id>", methods=["GET", "POST"])
-def article_delete(article_id):
-    if request.method == "GET":
-        return render_template("article/delete.html", article_id=article_id)
-    elif request.method == "POST":
-        article_id = request.form["article_id"]
-        article = Article.query.get(article_id)
-        db.session.delete(article)
-        db.session.commit()
-
-        flash(u"게시글 지웠다능..ㅠㅠ", "success")
-        return redirect(url_for("index"))
 
 
 @app.route('/sign_up', methods = ['GET', 'POST'])
@@ -336,7 +240,7 @@ def google_login():
         db.session.add(user)
         db.session.commit()
 
-        userdata = User.query.filter(User.google_id == google_userinfo['id']).first()
+        userdata = User.query.filter(User.googleogle_id == google_userinfo['id']).first()
         session["user_email"] = userdata.email
         session["user_name"] = userdata.username
         session["user_id"] = userdata.id
