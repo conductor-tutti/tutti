d104ee841fce1c1ce5acfd44f23d262e58a45e3f
Seulkiming <conductor@tutti.kr>
2014-09-18 19:12:59 +0900

    new variables for enabling Facebook OAuth login

---
+++ app/settings.py
@@ -0,0 +1,14 @@
+class Config(object):
+    SECRET_KEY = "xrdtfvbyuhnjimuygtfrdessdfnhhmjjygh65hrytrytr"
+    debug = True
+
+
+class Production(Config):
+    debug = True
+    CSRF_ENABLED = False
+    ADMIN = "tutti.conductor@gmail.com"
+    SQLALCHEMY_DATABASE_URI = "mysql+gaerdbms:///tuttidb?instance=tutti-alpha-seulki:tuttinstance"
+    migration_directory = "migrations"
+    # give your own values to 2 variables below
+    FACEBOOK_APP_ID = "944323692250762"
+    FACEBOOK_APP_SECERET = "207996ac76593a7fe2dd9930ecb79ce5"
