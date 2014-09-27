00563abd29a54b1c6682ed000a4a13e8ffe0e5dc
Seulkiming <conductor@tutti.kr>
2014-09-27 16:55:10 +0900

    video column was added to User table, but I refused to create it at that time. But Juneseok will do it

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(db.Model):
     # just forget deleting accounts until 'real' launching
     ## is_out = db.Column(db.Integer, default=0)    
 
+
 class Musician(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"))
@@ -31,7 +32,6 @@ class Musician(db.Model):
     education = db.Column(db.String(255))
     repertoire = db.Column(db.String(255))
     photo = db.Column(db.String(255)) # blob key lives in here
-
     created_on = db.Column(db.DateTime, default=db.func.now())
     updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
@@ -40,6 +40,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40))
     upper_id = db.Column(db.Integer)
+
 
 class UserRelationship(db.Model):
     id = db.Column(db.Integer, primary_key=True)
