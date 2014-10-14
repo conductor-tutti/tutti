e8b75516392c0f1aac297e5bafbd80ec7df7977c
Seulkiming <conductor@tutti.kr>
2014-10-15 02:49:20 +0900

    #24 still having trouble with deleting input fields

--- app/static/js/musician_new.js
+++ app/static/js/musician_new.js
@@ -56,6 +56,105 @@ $(document).ready(function(){
             }
         });
     });
+
+    var max_fields = 50;
+    var wrapper = $(".educationGroup");
+    var add_button = $("#addEducationInput");
+    var x = 1;
+    $(add_button).click(function(e){
+        e.preventDefault();
+        if(x < max_fields){
+            x++;
+            console.log(x)
+            $(wrapper).append("\
+            <div class='row newEducationRow'>\
+                <div class='form-group col-md-6 col-md-offset-3'>\
+                    <div class='input-group'>\
+                        <input class='form-control' placeholder='추가 학력사항'>\
+                        <span class='input-group-btn'>\
+                            <button id='delEducationInput' type='button' class='btn btn-default'><i class='fa fa-minus'></i></button>\
+                        </span>\
+                    </div>\
+                </div>\
+            </div>");
+        }
+    });
+
+    $(wrapper).on("click", "#delEducationInput", function(e){
+        e.preventDefault(); $(this).parent("div").remove(); x--;
+    });
+
+    // $("#addEducationInput").click(function(){
+    //     // adding input fields dynamically
+    //     $('.educationGroup').append("\
+    //         <div class='row newEducationRow'>\
+    //             <div class='form-group col-md-6 col-md-offset-3'>\
+    //                 <div class='input-group'>\
+    //                     <input class='form-control' placeholder='추가 학력사항'>\
+    //                     <span class='input-group-btn'>\
+    //                         <button id='delEducationInput' type='button' class='btn btn-default'><i class='fa fa-minus'></i></button>\
+    //                     </span>\
+    //                 </div>\
+    //             </div>\
+    //         </div>");
+    // });
+
+    // $("#newEducationRow").click(function(){
+    //     $(".newEducationRow").remove();
+    // });
+
+    $("#repertoire_create").click(function(){
+        $('.repertoire').append("<div><input type='text' name='repertoire_data'></div>")
+    });
+    
+    $("#repertoire_delete").click(function(){
+        $.ajax({
+            url:'/repertoire_delete',
+            type: 'POST',
+            datatype: "JSON",
+            data:{
+                data_repertoire_id: $('#repertoire_delete').attr("data_repertoire_id") 
+            },
+            success: function(data){
+                if (data.success){
+                    $("#repertoire_"+data.repertoire_id+"").remove()
+                    console.log('send msg success!');
+                }
+                else{
+                    console.log('send msg fail!');
+                }
+            },
+            error: function(data){
+                console.log("Server error!");
+            }
+        });
+    });
+    $("#video_create").click(function(){
+        $('.video').append("<div><input type='text' name='video_data'></div>")
+    });
+    $("#video_delete").click(function(){
+        $.ajax({
+            url:'/video_delete',
+            type: 'POST',
+            datatype: "JSON",
+            data:{
+                data_video_id: $('#video_delete').attr("data_video_id") 
+            },
+            success: function(data){
+                if (data.success){
+                    $("#video_"+data.video_id+"").remove()
+                    console.log('send msg success!');
+                }
+                else{
+                    console.log('send msg fail!');
+                }
+            },
+            error: function(data){
+                console.log("Server error!");
+            }
+        });
+    });
+
 });
 
 
