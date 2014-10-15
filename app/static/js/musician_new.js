0ba12fcd526e8724633c71ff0a130139150af3d7
Seulkiming <conductor@tutti.kr>
2014-10-16 00:38:58 +0900

    created two important functions, addInputField() and delInputField()

--- app/static/js/musician_new.js
+++ app/static/js/musician_new.js
@@ -1,3 +1,27 @@
+function addInputField(event) {
+    var identifier = event.data.identifier;
+    var wrapper = event.data.wrapper;
+
+    $(wrapper).append("\
+        <div class='row addedRow'>\
+        <div class='form-group col-md-6 col-md-offset-3'>\
+        <div class='input-group'>\
+        <input class='form-control'>\
+        <span class='input-group-btn'>\
+        <button type='button' class='delInput btn btn-default'><i class='fa fa-minus'></i></button>\
+        </span>\
+        </div>\
+        </div>\
+        </div>");
+    $(".delInput").click(delInputField);
+}
+
+function delInputField(event){
+    alert("clicked!")
+    $(this).parents(".row").remove();
+    console.log(this);
+}
+
 $(document).ready(function(){
     console.log("Hi! I'm musician_new.js");
 
@@ -57,32 +81,29 @@ $(document).ready(function(){
         });
     });
 
-    var max_fields = 50;
-    var wrapper = $(".educationGroup");
-    var add_button = $("#addEducationInput");
-    var x = 1;
-    $(add_button).click(function(e){
-        e.preventDefault();
-        if(x < max_fields){
-            x++;
-            console.log(x)
-            $(wrapper).append("\
-            <div class='row newEducationRow'>\
-                <div class='form-group col-md-6 col-md-offset-3'>\
-                    <div class='input-group'>\
-                        <input class='form-control' placeholder='추가 학력사항'>\
-                        <span class='input-group-btn'>\
-                            <button id='delEducationInput' type='button' class='btn btn-default'><i class='fa fa-minus'></i></button>\
-                        </span>\
-                    </div>\
-                </div>\
-            </div>");
-        }
-    });
+    // var max_fields = 50;
+    // var wrapper = $(".educationGroup");
+    // var add_button = $("#addEducationInput");
+    // var x = 1;
+    // $(add_button).click(function(e){
+    //     e.preventDefault();
+    //     if(x < max_fields){
+    //         x++;
+    //         console.log(x)
+    //         $(wrapper).append("\
+    //             <div class='row newEducationRow'>\
+    //             <div class='form-group col-md-6 col-md-offset-3'>\
+    //             <div class='input-group'>\
+    //             <input class='form-control' placeholder='추가 학력사항'>\
+    //             <span class='input-group-btn'>\
+    //             <button id='delEducationInput' type='button' class='btn btn-default'><i class='fa fa-minus'></i></button>\
+    //             </span>\
+    //             </div>\
+    //             </div>\
+    //             </div>");
+    //     }
+    // });
+$("#addEducationInput").click({identifier:'Education', wrapper:$(".educationGroup")}, addInputField);
 
-    $(wrapper).on("click", "#delEducationInput", function(e){
-        e.preventDefault();
-        $(this).parents(".row").remove();
-        x--;
-    });
 });
+
