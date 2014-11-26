8a3401b29c0af843f9d6c8dc901db2987e087caf
Seulkiming <conductor@tutti.kr>
2014-11-27 00:51:45 +0900

    fixed a bug in subcategory option

--- app/static/js/musician_new.js
+++ app/static/js/musician_new.js
@@ -56,59 +56,59 @@ $(document).ready(function(){
 
     $('#upperCategory').change(function() {
         console.log("You gave me upperCategory data.");
-
-        $.ajax({
-            url:'/musician/musician_category/',
-            type: 'POST',
-            dataType: 'JSON',
-            data:{
-                uppercategory_data: $('#upperCategory').val()
-            },
-            success: function(data) {
-
-                subcategory_option = "<option value='none'>선택하세요</option>";
-
-                console.log("success!");
-                for (var i = 0; i < data.subcategories.length; i++) {
-                    subcategory_option += '<option class="subcategory" value=' + data.subcategories[i][0] + '>' + data.subcategories[i][1] + '</option>';
+        if ($('#upperCategory').val() == 'none'){
+            subcategory_option = "<option value='none'>선택하세요</option>";
+            $("#subCategory").html(subcategory_option);
+        } else {
+            $.ajax({
+                url:'/musician/musician_category/',
+                type: 'POST',
+                dataType: 'JSON',
+                data:{
+                    uppercategory_data: $('#upperCategory').val()
+                },
+                success: function(data) {
+                    console.log("success!");
+                    subcategory_option = "<option value='none'>선택하세요</option>";
+                    for (var i = 0; i < data.subcategories.length; i++) {
+                        subcategory_option += '<option class="subcategory" value=' + data.subcategories[i][0] + '>' + data.subcategories[i][1] + '</option>';
+                    }
+                    $("#subCategory").html(subcategory_option);
+                    console.log($('.subcategory'))
+                },
+                error: function(e) {
+                    console.log('Something is wrong :(');
                 }
-                console.log($('.subcategory'))
-                
-                $("#subCategory").html(subcategory_option);
-                
-            },
-            error: function(e) {
-                console.log('Something is wrong :(');
-            }
-        });
+            });
+        }
     });
-    
-    $('#upperLocation').change(function() {
-        console.log("You gave me upperLocation data.");
 
-        $.ajax({
-            url:'/musician/musician_location/',
-            type: 'POST',
-            dataType: 'JSON',
-            data:{
-                upperlocation_data :$('#upperLocation').val()
-            },
-            success: function(data) {
+$('#upperLocation').change(function() {
+    console.log("You gave me upperLocation data.");
 
-                sublocation_option = "<option value='none'>선택하세요</option>";
+    $.ajax({
+        url:'/musician/musician_location/',
+        type: 'POST',
+        dataType: 'JSON',
+        data:{
+            upperlocation_data :$('#upperLocation').val()
+        },
+        success: function(data) {
 
-                console.log("success!");
-                for (var i = 0; i < data.sublocations.length; i++) {
-                    sublocation_option += '<option class="sublocation" value=' + data.sublocations[i][0] + '>' + data.sublocations[i][1] + '</option>';
-                }
-                console.log($('.sublocation'))                
-                $("#subLocation").html(sublocation_option);
-            },
-            error: function(e) {
-                console.log('Something is wrong :(');
+            sublocation_option = "<option value='none'>선택하세요</option>";
+
+            console.log("success!");
+            for (var i = 0; i < data.sublocations.length; i++) {
+                sublocation_option += '<option class="sublocation" value=' + data.sublocations[i][0] + '>' + data.sublocations[i][1] + '</option>';
             }
-        });
+            console.log($('.sublocation'))                
+            $("#subLocation").html(sublocation_option);
+        },
+        error: function(e) {
+            console.log('Something is wrong :(');
+        }
     });
+});
 
 $("#addEducationInput, #addRepertoireInput, #addVideoInput").click(addInputField);
 $(".delInput").click(delInputField);
